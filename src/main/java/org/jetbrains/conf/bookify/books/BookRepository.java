41afcdc275bb6a75fff6669629efe4737a8dd06c
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-08-06 10:19:06 +0200

    Formatting

--- src/main/java/org/jetbrains/conf/bookify/books/BookRepository.java
+++ src/main/java/org/jetbrains/conf/bookify/books/BookRepository.java
@@ -6,5 +6,6 @@ import java.util.List;
 import java.util.UUID;
 
 interface BookRepository extends CrudRepository<Book, UUID> {
+
     List<Book> findByNameContainingIgnoreCase(String name);
 }
--- src/main/java/org/jetbrains/conf/bookify/members/MemberRepository.java
+++ src/main/java/org/jetbrains/conf/bookify/members/MemberRepository.java
@@ -6,7 +6,10 @@ import java.util.List;
 import java.util.UUID;
 
 interface MemberRepository extends CrudRepository<Member, UUID> {
+
     List<Member> findByNameContainingIgnoreCase(String name);
+
     List<Member> findByEmailContainingIgnoreCase(String email);
+
     List<Member> findByEnabled(boolean enabled);
 }
