053a0e4baa7ee8d3674c7b2840ad42cd874beed0
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-10-23 17:05:12 +0200

    Improved AOT configuration by leveraging the latest framework changes

--- src/main/java/org/jetbrains/conf/bookify/config/AotConfiguration.java
+++ src/main/java/org/jetbrains/conf/bookify/config/AotConfiguration.java
@@ -2,22 +2,11 @@ package org.jetbrains.conf.bookify.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jdbc.core.convert.JdbcConverter;
-import org.springframework.data.jdbc.core.convert.MappingJdbcConverter;
 import org.springframework.data.jdbc.core.dialect.JdbcDialect;
 import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
-import org.springframework.data.relational.core.mapping.RelationalMappingContext;
 
-@Profile("aot")
 @Configuration
 public class AotConfiguration {
-
-    @Bean
-    JdbcConverter jdbcConverter(RelationalMappingContext context) {
-        return new MappingJdbcConverter(context, (identifier, path) -> null);
-    }
-
     @Bean
     JdbcDialect dialect() {
         return JdbcPostgresDialect.INSTANCE;
