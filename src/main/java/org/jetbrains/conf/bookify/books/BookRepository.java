35ccac0f41ac2bf70ba863636eeb65910c14136f
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-10-23 18:25:05 +0200

    Implemented AOT for spring-data-jpa

--- src/main/java/org/jetbrains/conf/bookify/books/BookRepository.java
+++ src/main/java/org/jetbrains/conf/bookify/books/BookRepository.java
@@ -1,16 +1,11 @@
 package org.jetbrains.conf.bookify.books;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.querydsl.binding.QuerydslPredicate;
-import org.springframework.data.relational.core.mapping.event.BeforeConvertCallback;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-interface BookRepository extends CrudRepository<Book, UUID> {
+interface BookRepository extends ListCrudRepository<Book, UUID> {
 
     List<Book> findByNameContainingIgnoreCase(String name);
 }
